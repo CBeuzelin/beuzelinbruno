558203736c61bc1ffc9ffa84915623772e06e591
Charly <charly.beuzelin@thalesgroup.com>
2023-03-13 11:39:51 +0100

    added navlink constants

---
+++ src/app/header/resources/constants/nav-link.constant.ts
@@ -0,0 +1,16 @@
+export const navLinks = {
+  home: '',
+  plomberie: {
+    _: 'plomberie',
+    plomberieSanitaire: 'plomberie-sanitaire',
+    creationRenovationSalleDeBain: 'creation-renovation-salle-de-bain',
+  },
+  chauffage: {
+    _: 'chauffage',
+    chaudieres: 'chaudieres',
+    pompesAChaleur: 'pompes-a-chaleur',
+    chauffageAuSol: 'chauffage-au-sol',
+    poeles: 'poeles',
+  },
+  contact: 'contact'
+}
